c331e7d437ba3e3aaf38e6a5f5cf5da6a34f4ed3
cocomodo <programmingonly17@gmail.com>
2023-05-04 22:57:28 +0900

    23.5.04

--- barkingDog Algorithm/10_DP/14002_가장 긴 증가하는 부분 수열 4 (myVer).cpp
+++ barkingDog Algorithm/10_DP/14002_가장 긴 증가하는 부분 수열 4 (myVer).cpp
@@ -23,6 +23,10 @@ int main() {
 			}
 		}
 	}
+  //순차적으로 maxD를 갱신하면서 그때의 i를 maxI로 최신화 하고,
+  //그 과정이 더이상 진행되지 않을때의 i를 maxI에 저장해서
+  //그 maxI을 시작점으로 뒤로 역순으로 수열의 원소들을 
+  //deque 혹은 vector에 넣었다가 프린트할 예정. 
 	int maxI = 1;
 	int maxD = d[1];
 	for (int i = 2; i <= n; i++) {
