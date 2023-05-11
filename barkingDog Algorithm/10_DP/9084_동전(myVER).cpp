cc5e7744f843303f91514ca80596a1ac86c78bf0
cocomodo <programmingonly17@gmail.com>
2023-05-12 00:45:01 +0900

    23.5.12

---
+++ barkingDog Algorithm/10_DP/9084_동전(myVER).cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+int a[25];
+int d[10'005];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 1; i <= n; i++)
+			cin >> a[i];
+		
+		int m;
+		cin >> m; //만들어야 할 금액, 동전 1 문제에서 k에 해당
+		d[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = a[i]; j <= m; j++) {
+				d[j] += d[j - a[i]];
+			}
+		}
+		cout << d[m] << '\n';
+
+		fill(d + 1, d + 1 + m, 0);
+	}
+}
--- barkingDog Algorithm/10_DP/9084_동전.cpp
+++ barkingDog Algorithm/10_DP/9084_동전.cpp
@@ -1,40 +1,25 @@
-// 3
-// 2
-// 1 2
-// 1000
-// 3
-// 1 5 10
-// 100
-// 2
-// 5 7
-// 22
-
+// Authored by : jihwan0123
+// Co-authored by : -
+// http://boj.kr/4b93b3cef4514d12be63f71be03ec2ee
 #include <bits/stdc++.h>
 using namespace std;
-
-int n, k;
-int a[10'005];
-int d[10'005];
-int t;
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin>>t;
-    while(t--){    
-        cin>>n;
-        for(int i=1;i<=n; i++)
-            cin>>a[i];
-        cin>>k;
-        d[0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=a[i];j<=k;j++){
-                d[j]+=d[j-a[i]];
-            }
-        }
-        cout<<d[k]<<'\n';
-        for(int i=1;i<=k;i++)
-            d[i]=0;
-    }
+int n, m;
+int a[10005], d[10005];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    fill(d, d + 10005, 0);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+      cin >> a[i];
+    cin >> m;
+    d[0] = 1;
+    for (int i = 0; i < n; i++)
+      for (int j = a[i]; j <= m; j++)
+        d[j] += d[j - a[i]]; // j - a[i]에 a[i] 동전 추가
+    cout << d[m] << '\n';
+  }
 }
