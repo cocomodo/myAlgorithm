badde3e4b77e8bc2b87292d798ac971e8d9a6145
장혁 <hyeok@janghyeog-ui-MacBookPro.local>
2023-05-12 13:32:29 +0900

    23.5.12

--- barkingDog Algorithm/10_DP/practice.cpp
+++ barkingDog Algorithm/10_DP/practice.cpp
@@ -1,31 +1,29 @@
-//3 10
-//1
-//2
-//5
-//첫째 줄에 n, k가 주어진다. (1 ≤ n ≤ 100, 1 ≤ k ≤ 10,000) 
-//다음 n개의 줄에는 각각의 동전의 가치가 주어진다. 동전의 가치는 100,000보다 
-//작거나 같은 자연수이다.
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int k;
-int a[105];
-int d[10'005];
+int n, m;
+int board[1005][1005];
+int d[1005][1005];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    cin>>n>>k;
-    for(int i=1; i<=n; i++)
-        cin>>a[i];
-    d[0]=1;
+    cin>>n>>m;
     for(int i=1; i<=n; i++){
-        for(int j=a[i];j<=k;j++){
-            d[j]+=d[j-a[i]];
+        string s;
+        cin>>s;
+        for(int j=1; j<=m;j++){
+            board[i][j]=s[j-1]-'0';
         }
     }
-    cout<<d[k];
+    int ans=0;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(board[i][j]==1){
+                d[i][j]=min({d[i-1][j],d[i][j-1],d[i-1][j-1]})+1;
+                ans=max(ans,d[i][j]);
+            }
+        }
+    }
+    cout<<ans*ans;
 }
