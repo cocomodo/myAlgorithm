85d2e437194778164dba797d0ca752f49f856476
장혁 <hyeok@janghyeog-ui-MacBookPro.local>
2023-05-15 00:04:48 +0900

    23.5.15

---
+++ barkingDog Algorithm/11.Greedy/11047_동전 0(myVER).cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int a[15];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+    
+    cin>>n>>k;
+    for(int i=1; i<=n; i++)
+        cin>>a[i];
+    
+    int ans=0;
+    int q=0;
+    int r=0;
+    for(int i=n; i>0;i--){
+        if(a[i]>k) continue;
+        if(k%a[i]==0) {
+            ans+=k/a[i];
+            cout<<ans;
+            return 0;
+        }
+        q=k/a[i];
+        r=k%a[i];
+        
+        ans+=q;
+        // k-=q*a[i];
+        k=r;
+
+    }
+}
