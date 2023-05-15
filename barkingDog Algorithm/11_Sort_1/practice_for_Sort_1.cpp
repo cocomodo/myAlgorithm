fec86a748567343bc5e855b082e69ac44eed2a7c
장혁 <hyeok@janghyeog-ui-MacBookPro.local>
2023-05-15 15:28:24 +0900

    23.5.15

--- barkingDog Algorithm/11_Sort_1/practice_for_Sort_1.cpp
+++ barkingDog Algorithm/11_Sort_1/practice_for_Sort_1.cpp
@@ -1,7 +1,7 @@
-int arr[5]={-2,2,4,6,13};
-int n=5;
-for(int i=0; i<n; i++){
-    for(int j=0; j<n-1-i;j++){
-        if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
-    }
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 }
