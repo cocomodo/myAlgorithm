eb6b22793ece925e2b305a6afedf3f8334777157
장혁 <hyeok@janghyeog-ui-MacBookPro.local>
2023-05-28 18:56:14 +0900

    23.5.28

--- barkingDog Algorithm/14_greedy/practice_for_Greedy.cpp
+++ barkingDog Algorithm/14_greedy/practice_for_Greedy.cpp
@@ -1,26 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a[105];
+bool power[105];
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n;
-    cin>>n;
-    vector<pair<int,int>> events;
-    for(int i=0; i<n; i++){
-        int l,r;
-        cin>>l>>r;
-        events.push_back({l,1});
-        events.push_back({r,-1});
-    }
-    sort(events.begin(),events.end());
+
+    int n, k;
+    cin>>n>>k;
+    for(int i=1; i<=k; i++) cin>>a[i];
+    int ans=0;
     int cnt=0;
-    int tot=0;
-    int loc=-1e9;
-    for(auto event: events){
-        if(cnt>0) tot+=event.first-loc;
-        cnt+=event.second;
-        loc=event.first;
+    for(int i=1;i<=k;i++){
+        int cur=a[i];
+        if(power[cur]) continue;
+        if(cnt<n){
+            power[cur]=true;
+            cnt++;
+        }
+        else{
+            vector<pair<int,int>> idx;
+            for(int x=1;x<=k;x++){
+                if(!power[x]) continue;
+                bool vis=false;
+                for(int y=i+1; y<=k; y++){
+                    if(a[y]==x){
+                        idx.push_back({y,x});
+                        vis=true;
+                        break;
+                    }
+                }
+                if(!vis) idx.push_back({k+1,x});
+            }
+            sort(idx.begin(),idx.end(),greater<pair<int,int>>());
+            int target=idx[0].second;//내림차순 정렬이기 때문에 이때의 0번째 인덱스가 가장 늦게 사용하게 될 전자제품이다. 
+            power[target]=false; ans++; // power를 false로 바꾸는 행위가 코드를 뽑는 일이다. 그리고 그렇게 되면 ans++;을 해주어야 한다. 
+            power[cur]=true;
+        }
     }
-    cout<<tot;
+    cout<<ans;
 }
