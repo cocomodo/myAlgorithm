4c6112098dfa61c0de14ffaa7eb4ee74a584d6fa
장혁 <hyeok@janghyeog-ui-MacBookPro.local>
2023-05-29 02:17:27 +0900

    23.5.29

--- barkingDog Algorithm/14_greedy/practice_for_Greedy.cpp
+++ barkingDog Algorithm/14_greedy/practice_for_Greedy.cpp
@@ -1,43 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[105];
-bool power[105];
-
+int n, c,m, ans;
+int truck[2002];
+vector<tuple<int,int,int>> orders;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, k;
-    cin>>n>>k;
-    for(int i=1; i<=k; i++) cin>>a[i];
-    int ans=0;
-    int cnt=0;
-    for(int i=1;i<=k;i++){
-        int cur=a[i];
-        if(power[cur]) continue;
-        if(cnt<n){
-            power[cur]=true;
-            cnt++;
-        }
-        else{
-            vector<pair<int,int>> idx;
-            for(int x=1;x<=k;x++){
-                if(!power[x]) continue;
-                bool vis=false;
-                for(int y=i+1; y<=k; y++){
-                    if(a[y]==x){
-                        idx.push_back({y,x});
-                        vis=true;
-                        break;
-                    }
-                }
-                if(!vis) idx.push_back({k+1,x});
+    cin>>n>>c>>m;
+    int f, t, b;
+    for(int i=0; i<m; i++){
+        cin>>f>>t>>b;
+        orders.push_back({t,f,b});
+    }
+    sort(orders.begin(),orders.end());
+    for(auto v: orders) {
+        tie(t,f,b)=v;
+        bool loadable=true;
+        for(int i=f; i<t; i++){
+            b=min(b,c-truck[i]);
+            if(!b){ //b==0이 되었다면, 
+                loadable=false;
+                break;
             }
-            sort(idx.begin(),idx.end(),greater<pair<int,int>>());
-            int target=idx[0].second;//내림차순 정렬이기 때문에 이때의 0번째 인덱스가 가장 늦게 사용하게 될 전자제품이다. 
-            power[target]=false; ans++; // power를 false로 바꾸는 행위가 코드를 뽑는 일이다. 그리고 그렇게 되면 ans++;을 해주어야 한다. 
-            power[cur]=true;
+        }
+        if(loadable){
+            ans+=b;
+            for(int i=f; i<t; i++) truck[i] +=b;
         }
     }
     cout<<ans;
