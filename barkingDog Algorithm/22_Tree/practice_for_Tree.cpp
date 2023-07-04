16a7ebd73d4ac22d01af782af035b511421cfe93
장혁 <hyeok@janghyeog-ui-MacBookPro.local>
2023-07-04 20:46:10 +0900

    23.7.4 맥북 설정 변경중

--- barkingDog Algorithm/22_Tree/practice_for_Tree.cpp
+++ barkingDog Algorithm/22_Tree/practice_for_Tree.cpp
@@ -1,42 +1,38 @@
-/*
-      1
-    (2) (3)
-   2     4
-           (2)
-            3
-*/
-/*이 문제의 경우는 트리의 각각의 간선들이 길이가 1이 아니라 다르게
-주어지는 문제이다. 
-*/
-/*이중배열을 선언해서 각각의 간선들이 가지는 값을 2중배열로 저장하면
-될것으로 보여진다 
-
-0 1 2 3 4 
-1 0 2 5 3
-2 2 0 7 5
-3 5 7 0 2 
-4 5 5 2 0
-
-*/
-
-/*
-4 2
-2 1 2
-4 3 2
-1 4 3
-1 2
-3 2
-
-*/
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-int lth[1'005][1'005];
-
+vector<pair<int,int>> adj[1'005];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
+
+    int n, m, u, v, d; //이때 d는 각각의 거리값
+    cin>>n>>m;
+    for(int i=0; i<n-1; i++){
+        cin>>u>>v>>d;
+        adj[u].push_back({v,d});
+        adj[v].push_back({u,d});
+    }
     
+    while(m--){
+        cin>>u>>v;
+        queue<pair<int,int>> q;
+        vector<bool> vis(n+1);
+        q.push({u,0});
+        vis[u]=true;
+        while(!q.empty()){
+            auto [cur,dist] =q.front();
+            q.pop();
+            if(cur==v){
+                cout<<dist<<'\n';
+                break;
+            }
+            for(auto [nxt,nxtDist]: adj[cur]){
+                if(vis[nxt]) continue;
+                vis[nxt]=true;
+                q.push({nxt,dist+nxtDist});
+            }
+        }
+    }
 }
