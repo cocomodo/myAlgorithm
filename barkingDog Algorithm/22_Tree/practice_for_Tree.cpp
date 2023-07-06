cd4c3d7bc960ac2bb055b2f40359c1612f6c6026
cocomodo <programmingonly17@gmail.com>
2023-07-06 22:04:16 +0900

    23.7.6 트리 해결중. 거의 다 해감

--- barkingDog Algorithm/22_Tree/practice_for_Tree.cpp
+++ barkingDog Algorithm/22_Tree/practice_for_Tree.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 int n, m;
-vector<int> adj[100'005];
-bool vis[100'005];
-int cnt;
+int p[100'005];
+int score[100'005];
+vector<int> child[100'005];
 
 void dfs(int cur){
-    vis[cur]=true;
-    for(auto nxt:adj[cur]){
-        if(vis[nxt]) continue;
-        dfs(nxt);
-    }
+    if(p[cur]!=-1)
+        score[cur]+=score[p[cur]];
+        for(int nxt:child[cur])
+            dfs(nxt);
 }
 
 int main(){
@@ -19,18 +18,16 @@ int main(){
     cin.tie(0);
 
     cin>>n>>m;
-    
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
     for(int i=1; i<=n; i++){
-        if(vis[i]) continue;
-        dfs(i);
-        cnt++;
+        cin>>p[i];
+        if(p[i]==-1) continue;
+        child[p[i]].push_back(i);
     }
-    cout<<cnt-1+(m+cnt-1)-(n-1);
-
+    while(m--){
+        int i, w;
+        cin>>i>>w;
+        score[i]+=w;
+    }
+    dfs(1);
+    for(int i=1; i<=n; i++) cout<<score[i]<<' ';
 }
